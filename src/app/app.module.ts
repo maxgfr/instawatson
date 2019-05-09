91b22af245095a212328eaf860af09963f79b79e
@maxgfr <maxgolfier@gmail.com>
2019-05-09 12:34:50 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { InstaComponent } from './insta/insta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/insta/insta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'insta',
+  templateUrl: './insta.component.html',
+  styleUrls: ['./insta.component.css']
+})
+export class InstaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

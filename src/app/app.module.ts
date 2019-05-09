78116dbef9cdd588ff5fa9d18e8dc1feab3aec31
@maxgfr <maxgolfier@gmail.com>
2019-05-09 15:20:56 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InstaComponent } from './insta/insta.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,8 +17,9 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
-    MatCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/insta/insta.component.ts
+++ src/app/insta/insta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'insta',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) {
+    this.activatedRoute.params.subscribe(params => {
+          const access_token = params['access_token'];
+          if(access_token) {
+            console.log(access_token)
+            const promises = [];
+            promises.push(this.http.get(`https://api.instagram.com/v1/users/self?access_token=${access_token}`).toPromise())
+            promises.push(this.http.get(`https://api.instagram.com/v1/users/self/media/recent?access_token=${access_token}`).toPromise())
+            Promise.all(promises)
+              .then((res) => {
+                console.log(res[0])
+                console.log(res[1])
+              })
+              .catch((error) => {
+                console.error(error.message || error);
+              });
+          }
+    });
+  }
 
   ngOnInit() {
   }
-
 }
